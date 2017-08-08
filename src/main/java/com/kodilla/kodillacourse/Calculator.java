66054e4af96c6d1b6bbce45966be926a23a57331
Tomasz Badelek <tbadelek@gmail.com>
2017-08-08 11:44:19 +0200

    2nd commit

--- src/main/java/com/kodilla/kodillacourse/Calculator.java
+++ src/main/java/com/kodilla/kodillacourse/Calculator.java
@@ -1,4 +1,24 @@
 package com.kodilla.kodillacourse;
 
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
 public class Calculator {
+
+    double a;
+    double b;
+
+
+    public double sumAandB(double a, double b){
+        return a + b;
+    }
+
+    public double substractBfromA(double a, double b){
+        return a - b;
+    }
+
+
+
 }
--- src/main/java/com/kodilla/kodillacourse/KodillaCourseApplication.java
+++ src/main/java/com/kodilla/kodillacourse/KodillaCourseApplication.java
@@ -6,10 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class KodillaCourseApplication {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
+        Calculator calculator = new Calculator();
 
+        double result = calculator.sumAandB(5.5, 1.4);
 
-
-	}
+        System.out.println(result);
+    }
 }
